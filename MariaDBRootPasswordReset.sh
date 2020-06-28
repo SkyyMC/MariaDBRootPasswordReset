a0a5152fb36c81640fb4dbf150356ab9941f5371
Skyy <43611579+SkyyMC@users.noreply.github.com>
2020-06-28 20:24:06 +0200

    Add files via upload

---
+++ MariaDBRootPasswordReset.sh
@@ -0,0 +1,15 @@
+output(){
+    echo -e '\e[36m'$1'\e[0m';
+}
+
+mariadb_root_reset(){
+    rootpassword=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`  
+    Q0="SET old_passwords=0;"
+    Q1="SET PASSWORD FOR root@localhost = PASSWORD('$rootpassword');"
+    Q2="FLUSH PRIVILEGES;"
+    SQL="${Q0}${Q1}${Q2}"
+    mysql mysql -e "$SQL"
+    output "Twoje haslo MariaDB to: $rootpassword"
+}
+
+mariadb_root_reset
